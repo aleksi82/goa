f874619608b167520d30ae748b4166224a92bdd5
aleksi82 <147658876+aleksi82@users.noreply.github.com>
2023-10-15 19:40:13 +0300

    Add files via upload

---
+++ homework/homework1.py
@@ -0,0 +1,35 @@
+score = 100
+
+temperature = 27
+
+date_type = "string"
+
+city = "paris"
+
+age = 56
+
+name = "tom"
+
+
+song = "let it be"
+
+items = 18
+boxes = 6
+
+print ("items / boxes")
+
+print (score)
+
+print (temperature)
+
+print (date_type)
+
+print (city)
+
+print (age)
+
+print (name)
+
+print (song)
+
+print (items / boxes)
